a03936b56870f85ffce97cf26457293c448f28c5
D3boker1 <fmarques_00@protonmail.com>
2024-03-11 10:26:20 +0000

    fix(setup): remove absolute exports

--- setup.sh
+++ setup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-echo "exporting RISCV"
+# echo "exporting RISCV"
 
-export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin/riscv32-unknown-elf:$PATH
+# export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin/riscv32-unknown-elf:$PATH
 
-export PATH=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0/bin:$PATH
+# export PATH=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0/bin:$PATH
 
-export RISCV=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0
+# export RISCV=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0
 
 export SW_HOME=$(pwd)/software
 
 export HW_HOME=$(pwd)/hardware
 
-echo "exporting QUESTASIM PATH"
+# echo "exporting QUESTASIM PATH"
 
-export QUESTASIM_HOME=/usr/pack/questa-2022.3-bt/questasim/
+# export QUESTASIM_HOME=/usr/pack/questa-2022.3-bt/questasim/
 
 echo "exporting RISCV 32 bit with zfinx"
 
-export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin:$PATH
+# export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin:$PATH
 
 echo "cloning submodules"
 
